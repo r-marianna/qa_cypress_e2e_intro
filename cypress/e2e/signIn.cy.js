a7397095f0c85cae65414aa84e149fd028293ce3
Marianna Repka <repka.mv@gmail.com>
2025-06-24 23:31:14 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,23 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  it('should provide an ability to log in and ' +
+    'contain username in the header after', () => {
+    cy.visit('/user/login');
 
+    cy.get('input[type="email"]')
+      .should('exist')
+      .type('moriarty@villainmail.com');
+
+    cy.get('input[type="password"]')
+      .should('exist')
+      .type('moriarty');
+
+    cy.get('button[type="Submit"]')
+      .should('exist')
+      .click();
+
+    cy.get('a.nav-link', { timeout: 1000 })
+      .should('contain.text', 'moriarty');
   });
 });
